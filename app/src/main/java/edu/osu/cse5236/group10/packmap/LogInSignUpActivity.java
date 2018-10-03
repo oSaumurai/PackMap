446f62676b7a3fed13611a163fa7b9ae962f61a0
Chen Bin <chen.6133@osu.edu>
2018-10-02 22:35:29 -0400

    Add LogInSignUp activity
    This activity contains two buttons: login button and sign up button.

---
+++ app/src/main/java/edu/osu/cse5236/group10/packmap/LogInSignUpActivity.java
@@ -0,0 +1,13 @@
+package edu.osu.cse5236.group10.packmap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LogInSignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in_sign_up);
+    }
+}
--- app/src/main/java/edu/osu/cse5236/group10/packmap/LoginActivity.java
+++ app/src/main/java/edu/osu/cse5236/group10/packmap/LoginActivity.java
@@ -3,6 +3,7 @@ package edu.osu.cse5236.group10.packmap;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -217,6 +218,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
             mAuthTask.execute((Void) null);
+
+            Log.d(TAG, TAG + ": User Log in!");
+            startActivity(new Intent(this, PackListActivity.class));
         }
     }
 
