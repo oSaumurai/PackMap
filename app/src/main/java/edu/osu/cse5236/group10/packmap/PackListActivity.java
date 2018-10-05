916af282cfb0e091755da2281c14c0c5c95c3994
Nic Siebenaller <n.7aller@gmail.com>
2018-10-05 00:30:21 -0400

    Updated pack list activity with placeholder RecyclerView

--- app/src/main/java/edu/osu/cse5236/group10/packmap/PackListActivity.java
+++ app/src/main/java/edu/osu/cse5236/group10/packmap/PackListActivity.java
@@ -1,15 +1,20 @@
 package edu.osu.cse5236.group10.packmap;
 
+import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class PackListActivity extends AppCompatActivity {
+import edu.osu.cse5236.group10.packmap.dummy.DummyContent;
+
+public class PackListActivity extends AppCompatActivity implements PackFragment.OnListFragmentInteractionListener {
 
     private TextView mTextMessage;
+    private PackFragment mPackFragment;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -18,13 +23,13 @@ public class PackListActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    mTextMessage.setText(R.string.title_packs);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    mTextMessage.setText(R.string.title_places);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    mTextMessage.setText(R.string.title_explore);
                     return true;
             }
             return false;
@@ -37,8 +42,13 @@ public class PackListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_pack_list);
 
         mTextMessage = (TextView) findViewById(R.id.message);
+        mPackFragment = (PackFragment) getSupportFragmentManager().findFragmentById(R.id.pack_list);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+
+    }
 }
