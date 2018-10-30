ee2c52ce772e0a60fb91822723dd6a2fa772cbc1
haifeng yang <571159037@qq.com>
2018-10-30 01:08:39 -0400

    Add current user Location

--- app/src/main/java/edu/osu/cse5236/group10/packmap/MapsActivity.java
+++ app/src/main/java/edu/osu/cse5236/group10/packmap/MapsActivity.java
@@ -2,6 +2,7 @@ package edu.osu.cse5236.group10.packmap;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.nfc.Tag;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -12,12 +13,17 @@ import android.util.Log;
 import android.widget.Switch;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.api.LogDescriptor;
 
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -28,7 +34,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private static final String COURSE_LOCAITON=Manifest.permission.ACCESS_COARSE_LOCATION;
     private static final int LOCATION_PERMISSION_REQUESTCODE=1234;
     private Boolean mLocationPermissionGranted=false;
-
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+    private static final float DEFAULT_ZOOM = 15f;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,10 +57,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         Log.d(Tag,"onMapReady: map is ready");
         mMap = googleMap;
 
+        if(mLocationPermissionGranted){
+            getDeviceLocation();
+            if(ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION)!=PackageManager.PERMISSION_GRANTED){
+                return;
+            }
+            mMap.setMyLocationEnabled(true);
+        }
+
         // Add a marker in Sydney, Australia, and move the camera.
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //LatLng sydney = new LatLng(-34, 151);
+        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
     private void getLocationPermission(){
@@ -68,6 +84,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     ==PackageManager.PERMISSION_GRANTED){
                 //set
                 mLocationPermissionGranted=true;
+                initMap();
             }else{
                 ActivityCompat.requestPermissions(this,permissions,LOCATION_PERMISSION_REQUESTCODE);
             }
@@ -77,6 +94,43 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     }
 
+    private void getDeviceLocation(){
+        Log.d(Tag,"getDeviceLocation: getting the current devices location");
+        mFusedLocationProviderClient= LocationServices.getFusedLocationProviderClient(this);
+
+        try{
+            if(mLocationPermissionGranted){
+                Task location=mFusedLocationProviderClient.getLastLocation();
+                Task task = location.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if (task.isSuccessful() && task != null) {
+                            Log.d(Tag, "onComplete: found locaiton");
+                            Location currentLocaiton = (Location) task.getResult();
+                            if (currentLocaiton != null) {
+                                moveCamera(new LatLng(currentLocaiton.getLatitude(), currentLocaiton.getLongitude()),
+                                        DEFAULT_ZOOM);
+                            } else {
+                                Log.d(Tag, "onComplete: null returned");
+                            }
+                        } else {
+                            Log.d(Tag, "onComplete: current locaiton is null");
+                            Toast.makeText(MapsActivity.this, "", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+
+        }catch(SecurityException e){
+            Log.e(Tag, "getDeviceLocation: SecurityExeption; "+e.getMessage());
+        }
+    }
+
+
+    private void moveCamera(LatLng latLng, float zoom){
+        Log.d(Tag, "moveCamera: moving the camera to: lat: " + latLng.latitude+", lng "+ latLng.longitude);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,zoom));
+    }
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         Log.d(Tag,"onRequestPermissionResult: called");
